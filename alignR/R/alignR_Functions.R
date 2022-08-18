1582c8fef9e9cc6a92c45698f7e4bd9f584e9d34
ZacharySMorris <zachary.morris@yale.edu>
2022-08-18 16:13:28 -0400

    working versions of readLandmarks and writeLandmarks

--- alignR/R/alignR_Functions.R
+++ alignR/R/alignR_Functions.R
@@ -1,8 +1,80 @@
 ### Functions for interacting with data from alignR ###
 
-# reading in data
+# writing out alignR landmark data (built off of list2XML4R in the StereoMorph package)
+writeLandmarks <- function(x, file, ind=0){
+  #x is a list or array of landmark data created in alignR
+
+  # x = lm_list
+
+  if(length(x) == 0) return()
+
+  str <- '' #create blank XML string to be built up
+
+  if(class(x) == "list"){
+
+    for(i in 1:length(x)){
+
+      name <- names(x)[i]
+
+      if(is.null(x[[name]])) next
+
+      if(class(x[[name]]) == "logical") {
+        str <- c(str, paste(paste(rep('\t', ind), collapse=''), '<', name,'>\n', sep = ""))
+        str <- c(str, paste("NA\n"))
+        str <- c(str, paste(paste(rep('\t', ind), collapse=''), '</', name,'>\n', sep = ""))
+      }
+
+      if(class(x[[name]]) == c('matrix',"array")){
+        if(!is.null(rownames(x[[name]])) || is.null(colnames(x[[name]]))){
+          row_names <- rownames(x[[name]])
+          col_names <- colnames(x[[name]])
+        } else{
+          row_names <- paste("LM",seq(nrow(x[[name]])),sep = "")
+          col_names <- c("X","Y","Z")[seq(ncol(x[[name]]))]
+        }
+
+        # OPENING TAG
+        str <- c(str, paste(paste(rep('\t', ind), collapse=''), '<', name,'>\n', sep = ""))
+        # SEND LIST RECURSIVELY
+        # str <- c(str, writeLandmarks(x[[name]], ind=ind+1))
+        str <- c(str, paste(paste(rep('\t', ind), collapse=''), '<type=matrix rownames=', paste(row_names,collapse = ","), ' colnames=', paste(col_names,collapse = ","),' nrow=', nrow(x[[name]]),' ncol=', ncol(x[[name]]),'>\n', sep = ""))
+
+        # str <- c(str, '\t', paste(rep('\t', ind+1), collapse=''))
+        # str <- c(str, '\t')
+        str <- c(str, paste0('\t\t',paste(col_names, collapse='\t'), '\n'))
+
+        # MATRIX VALUES
+        for(r in 1:nrow(x[[name]])){
+          # str <- c(str, '\t', paste(rep('\t', ind), collapse=''))
+          str <- c(str, paste0('\t',row_names[r],'\t',paste(x[[name]][r, ], collapse='\t'), '\n'))
+        }
+        str <- c(str, paste(paste(rep('\t', ind), collapse=''), '</', name,'>\n', sep = ""))
+      }
+      }
+
+    }
+
+    if (any(unlist(lapply(x,class)) == "logical") || !all(unlist(sapply(x,class)) == c("matrix","array"))){
+      warning("Some specimens lack a matrix of landmark data.")
+      }
+
+  # if(class(x) == "array"){
+  #
+  # }
+
+  # str <- paste(sub("\n$","",str), collapse='')
+  str <- sub("\n$", "", paste(str, collapse=''))
+
+  if(file != "") write(str, file)
+
+}
+#
+
+# reading in alignR landmark data
 readLandmarks <- function(x){
   #x is the name of an alignR landmarks file
+
+  # x<-c("Write_Test.txt")
 
   file <- readLines(x)
   lines <- sub("\t", "", file)
@@ -20,17 +92,21 @@ readLandmarks <- function(x){
     }
     tmp_rows <- seq(tmp_ends[1]+1,tmp_ends[2]-1)
     tmp_lines <- lines[tmp_rows] #save single specimen lines
-    tmp_data <- grep("^\t", sub("\t", "", grep("^\t", tmp_lines,value=T)),value=T,invert=T) #subset to the rows with landmark data + rownames
+    if(length(tmp_lines)==1 && "NA"==tmp_lines){
+      landmark_df[[tmp_id]] <- tmp_lines
+      next
+    }
+    tmp_data <- grep("^<type|^\t",tmp_lines,value=T,invert=T) #subset to the rows with landmark data + rownames
     tmp_var <- strsplit(grep("<[type=[:alnum:]|_.]+[ ]", tmp_lines,value=T)," ") #split lines that identify type into parts
 
     row_n <- as.numeric(unique(gsub("nrow=","",grep("nrow=",unlist(tmp_var),value=T)))) #saves number of rows
-    row_lab <- unique(grep("[[:alpha:]]",unlist(strsplit(tmp_data,"\t")),value=T)) #this grabs the rownames
+    row_lab <- unlist(strsplit(gsub("rownames=","",grep("rownames=",unlist(tmp_var),value=T)),",")) #this grabs the rownames
 
-    col_n <- as.numeric(unique(gsub("ncol=","",grep("ncol=",unlist(tmp_var),value=T)))) #saves number of columns
-    col_lab <- unlist(strsplit(gsub("^\t\t","", grep("^\t\t", tmp_lines,value=T)),"\t")) #grab and prep colnames
+    col_n <- as.numeric(unique(gsub("ncol=|>","",grep("ncol=",unlist(tmp_var),value=T)))) #saves number of columns
+    col_lab <- unlist(strsplit(gsub("colnames=","",grep("colnames=",unlist(tmp_var),value=T)),",")) #grab and prep colnames
 
     pre_matrix <- as.numeric(unlist(strsplit(sub("\\w*\t", "", tmp_data),"\t")))
-    tmp_matrix <- matrix(pre_matrix,ncol=3,byrow = T,dimnames =list(row_lab,col_lab))
+    tmp_matrix <- matrix(pre_matrix,ncol=col_n,byrow = T,dimnames =list(row_lab,col_lab))
     tmp_matrix
 
     row_list[[tmp_id]] <- row_lab
