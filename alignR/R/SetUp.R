ace95838d4948ad32b96c5a852f76ec35bc07eba
Zachary S. Morris <62260036+ZacharySMorris@users.noreply.github.com>
2022-11-10 13:28:12 -0500

    Update SetUp.R
    Commenting out so it will install from GitHub

--- alignR/R/SetUp.R
+++ alignR/R/SetUp.R
@@ -1,51 +1,51 @@
 ### Loading packages ###
 
-library(geomorph)
-library(shiny)
-library(shinyWidgets)
-library(shinyFiles)
-library(shinyjs)
-library(shinyalert)
-library(shinyBS)
-library(shinyFeedback)
-library(shinycssloaders)
-library(fontawesome)
-library(Rvcg)
-library(Morpho)
-library(scales)
-library(tools)
-library(sp)
-library(crosstalk)
-library(rmarkdown)
+#library(geomorph)
+#library(shiny)
+#library(shinyWidgets)
+#library(shinyFiles)
+#library(shinyjs)
+#library(shinyalert)
+#library(shinyBS)
+#library(shinyFeedback)
+#library(shinycssloaders)
+#library(fontawesome)
+#library(Rvcg)
+#library(Morpho)
+#library(scales)
+#library(tools)
+#library(sp)
+#library(crosstalk)
+#library(rmarkdown)
 
-usethis::use_package("shiny")
-usethis::use_package("shinyjs")
-usethis::use_package("shinyFeedback")
-usethis::use_package("fontawesome")
-usethis::use_package("Morpho")
-usethis::use_package("tools")
-usethis::use_package("rgl")
-usethis::use_package("Rvcg")
+#usethis::use_package("shiny")
+#usethis::use_package("shinyjs")
+#usethis::use_package("shinyFeedback")
+#usethis::use_package("fontawesome")
+#usethis::use_package("Morpho")
+#usethis::use_package("tools")
+#usethis::use_package("rgl")
+#usethis::use_package("Rvcg")
 
 
 # spec.lo <- c("/Users/zach/Dropbox/AmniotePalateDiversity/Palatine_Meshes")
-spec.lo <- c("~/Dropbox/alignR/alignR/data/turts/input/")
+#spec.lo <- c("~/Dropbox/alignR/alignR/data/turts/input/")
 
 #This will be the ultimate wrapper for the function, which takes a file directory and then passes the list of objects to the app
 #Doesn't work just yet
-alignR(file_dir = spec.lo, file_name = "TurtTest.txt")
+#alignR(file_dir = spec.lo, file_name = "TurtTest.txt")
 
-turt_test <- readLandmarks("Write_Test.txt")
+#turt_test <- readLandmarks("Write_Test.txt")
 
-rgl.setMouseCallbacks()
+#rgl.setMouseCallbacks()
 
 ##font awesome icon for R
 # <i class="fa-brands fa-r-project"></i>
 
-turt_test.gpa <- gpagen(turt_test)
+#turt_test.gpa <- gpagen(turt_test)
 
-turt_test.gpa$coords
+#turt_test.gpa$coords
 
-turt_test.pca <- gm.prcomp(turt_test.gpa$coords)
+#turt_test.pca <- gm.prcomp(turt_test.gpa$coords)
 
-plot(turt_test.pca$x[,1],turt_test.pca$x[,2])
+#plot(turt_test.pca$x[,1],turt_test.pca$x[,2])
