8862e9961ad3a2349c96b9fd59cffa452b4fa63b
ZacharySMorris <Zachary.Morris@yale.edu>
2022-11-17 15:11:14 -0500

    Re-organizing package structure.

---
+++ R/alignR-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import shiny
+#' @import shinyjs
+#' @import shinyFeedback
+#' @import rgl
+#' @import fontawesome
+#' @import Morpho
+#' @import tools
+#' @import Rvcg
+## usethis namespace: end
+NULL
